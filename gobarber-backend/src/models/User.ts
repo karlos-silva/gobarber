437170c1ea216bd99f2f1d31c69f64b0e64b9f1e
Karlos Silva <karlossilva194@gmail.com>
2020-11-26 02:46:28 -0300

    Adiciona migrations de User e cria Model Users

--- gobarber-backend/src/models/User.ts
+++ gobarber-backend/src/models/User.ts
@@ -4,27 +4,23 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
-  ManyToOne,
-  JoinColumn,
 } from 'typeorm';
 
-import User from './User';
+// KISS - Keep it Simple & Stupid
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  // eslint-disable-next-line camelcase
-  provider_id: string;
+  name: string;
 
-  @ManyToOne(() => User)
-  @JoinColumn({ name: 'provider_id' })
-  provider: User;
+  @Column()
+  email: string;
 
-  @Column('time with time zone')
-  date: Date;
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   // eslint-disable-next-line camelcase
@@ -35,4 +31,4 @@ class Appointment {
   updated_at: Date;
 }
 
-export default Appointment;
+export default User;
